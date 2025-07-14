b58e59a354b46fa136ecb63a21d6df8639c42518
Dima2024-python <chagaydapython2024@gmail.com>
2025-07-14 20:30:32 +0300

    homework 5

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import homework_querysets
 
 urlpatterns = [
-    path('homework/querysets/', homework_querysets),
+    path('homework/querysets/', homework_querysets, name='homework_querysets'),
 ]
